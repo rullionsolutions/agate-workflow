bbc724b0180bc919518a93797e20d02c200d6761
Stephen Francis <stephen.francis@rullion.co.uk>
2017-03-30 11:17:41 +0100

    move one time WF task guest access logic out of UI.Page

--- wf_inst_node/ac_wf_inst_node_one_time_exit.js
+++ wf_inst_node/ac_wf_inst_node_one_time_exit.js
@@ -26,3 +26,16 @@ module.exports.defbind("setupEnd", "setupEnd", function () {
         "You can now close this window/tab or click the button below to login to myRecruiter.<br/><br/>" +
         "<a href=\"./index.html\" id=\"login\" class=\"btn css_cmd\" style=\"margin-right:20px;\">Go to Log-in</a>";
 });
+
+
+UI.Page.defbind("setExitURLonOneTimeExit", "setupStart", function () {
+    var exit_url_key;
+    if (this.allowed && this.allowed.one_time_guest_wf_access) {
+        this.skin = "guest.html";
+        // Redirect to 'thank you' page if one time guest access
+        if (this.performing_wf_nodes && this.performing_wf_nodes[0]) {
+            exit_url_key = this.performing_wf_nodes[0].getKey();
+        }
+        this.exit_url_save = UI.pages.get("wf_inst_node_one_time_exit").getSimpleURL(exit_url_key);
+    }
+});
